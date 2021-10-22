a223d5d531bdd74e6d5381bad213b57c08621b58
Darren Wong <wongdarren1997@gmail.com>
2021-10-22 17:24:47 -0500

    Add navigation

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
+import Navbar from './navbar';
 import { Box, Container } from '@chakra-ui/react';
+import { Router } from 'next/dist/client/router';
 
 const Main = ({ children }) => {
   return (
@@ -8,6 +10,8 @@ const Main = ({ children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />{' '}
         <title>Darren Wong - Home</title>{' '}
       </Head>
+
+      <Navbar path={Router.asPath} />
 
       <Container maxW="container.md" pt={14}>
         {children}
